2b33943f9be5c65af2b79415314fe6db4fde2ec9
Malte Jörgens <malte@stbjoergens.de>
2020-12-02 12:46:56 +0100

    Day 2 Part 1

--- 2/2.go
+++ 2/2.go
@@ -7,12 +7,13 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type Policy struct {
-	MinOccurs uint
-	MaxOccurs uint
-	Character byte
+	MinOccurs int
+	MaxOccurs int
+	Character string
 }
 
 type passwordWithPolicy struct {
@@ -21,11 +22,18 @@ type passwordWithPolicy struct {
 }
 
 func PasswordValid(policy Policy, password string) bool {
-	return false
+	count := strings.Count(password, policy.Character)
+	return count <= policy.MaxOccurs && count >= policy.MinOccurs
 }
 
-func countValidPasswords([]passwordWithPolicy) uint {
-	return 0
+func countValidPasswords(inputs []passwordWithPolicy) uint {
+	var count uint = 0
+	for _, v := range inputs {
+		if PasswordValid(v.policy, v.password) {
+			count++
+		}
+	}
+	return count
 }
 
 // https://adventofcode.com/2020/day/2
@@ -58,14 +66,14 @@ func readInput(path string) ([]passwordWithPolicy, error) {
 	for scanner.Scan() {
 		line := scanner.Text()
 		match := re.FindStringSubmatch(line)
-		minOccursString, err1 := strconv.ParseUint(match[1], 10, 64)
-		maxOccursString, err2 := strconv.ParseUint(match[2], 10, 64)
+		minOccursString, err1 := strconv.ParseInt(match[1], 10, 64)
+		maxOccursString, err2 := strconv.ParseInt(match[2], 10, 64)
 		if match == nil || err1 != nil || err2 != nil {
 			return nil, errors.New("Invalid line: " + line)
 		}
-		minOccurs := uint(minOccursString)
-		maxOccurs := uint(maxOccursString)
-		char := match[3][0]
+		minOccurs := int(minOccursString)
+		maxOccurs := int(maxOccursString)
+		char := match[3]
 		password := match[4]
 
 		result = append(result, passwordWithPolicy{Policy{minOccurs, maxOccurs, char}, password})
--- 2/2_test.go
+++ 2/2_test.go
@@ -5,8 +5,30 @@ import (
 )
 
 func TestPassword1(t *testing.T) {
-	policy := Policy{1, 3, 'a'}
+	policy := Policy{1, 3, "a"}
 	password := "abcde"
+
+	output := PasswordValid(policy, password)
+
+	if !output {
+		t.Fatalf(`expected true, got false`)
+	}
+}
+
+func TestPassword2(t *testing.T) {
+	policy := Policy{1, 3, "b"}
+	password := "cdefg"
+
+	output := PasswordValid(policy, password)
+
+	if output {
+		t.Fatalf(`expected false, got true`)
+	}
+}
+
+func TestPassword3(t *testing.T) {
+	policy := Policy{2, 9, "c"}
+	password := "ccccccccc"
 
 	output := PasswordValid(policy, password)
 
