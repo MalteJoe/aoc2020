d50555f8354757c71324d2707129110f3ba9f412
Malte Jörgens <malte.joergens@wps.de>
2020-12-14 08:26:20 +0100

    Day 14 Part 1

---
+++ 14/14.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Challenge
+type Input []Instruction
+type Instruction struct {
+	Lvalue string
+	RValue string
+}
+
+type state struct {
+	mask0 uint64
+	mask1 uint64
+	mem   map[uint64]uint64
+}
+
+func ReadInput(input io.Reader) (result Input, err error) {
+	scanner := bufio.NewScanner(input)
+	result = make(Input, 0)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " = ")
+		result = append(result, Instruction{parts[0], parts[1]})
+	}
+
+	err = scanner.Err()
+
+	return
+}
+
+// Execute the initialization program.
+// What is the sum of all values left in memory after it completes?
+func Part1(input Input) (result int) {
+	state := state{1<<36 - 1, 0, make(map[uint64]uint64)}
+	for _, cmd := range input {
+		if cmd.Lvalue == "mask" {
+			state.mask0, _ = strconv.ParseUint(strings.ReplaceAll(cmd.RValue, "X", "1"), 2, 36)
+			state.mask1, _ = strconv.ParseUint(strings.ReplaceAll(cmd.RValue, "X", "0"), 2, 36)
+		} else {
+			addr, _ := strconv.ParseUint(cmd.Lvalue[4:len(cmd.Lvalue)-1], 10, 36)
+			value, _ := strconv.ParseUint(cmd.RValue, 10, 36)
+			state.mem[addr] = value&state.mask0 | state.mask1
+		}
+	}
+	for _, v := range state.mem {
+		result += int(v)
+	}
+	return
+}
+
+// TODO
+func Part2(input Input) (result int) {
+	return -1
+}
+
+// https://adventofcode.com/2020/day/14
+func main() {
+	log.SetPrefix("14: ")
+	log.SetFlags(log.Ltime)
+
+	file, err := os.Open("./input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	input, err := ReadInput(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Answer Part 1: %v", Part1(input))
+	log.Printf("Answer Part 2: %v", Part2(input))
+
+}
+
+// utils
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func sum(slice []int) (sum int) {
+	for _, v := range slice {
+		sum += v
+	}
+	return
+}
+
+func contains(slice []int, value int) bool {
+	return indexOf(slice, value) != -1
+}
+
+func indexOf(slice []int, value int) int {
+	for i, v := range slice {
+		if v == value {
+			return i
+		}
+	}
+	return -1
+}
+
+func min(slice []int) (min int) {
+	if len(slice) == 0 {
+		log.Fatal("min of empty slice")
+	}
+	min = slice[0]
+	for _, v := range slice[1:] {
+		if v < min {
+			min = v
+		}
+	}
+	return
+}
+
+func max(slice []int) (max int) {
+	if len(slice) == 0 {
+		log.Fatal("max of empty slice")
+	}
+	max = slice[0]
+	for _, v := range slice[1:] {
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
+
+func parseInt(s string) int {
+	result, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("Not an int: %s", s)
+	}
+	return result
+}
---
+++ 14/14_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+// Tests
+func TestPart1(t *testing.T) {
+	input := `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0`
+
+	parsed, err := ReadInput(strings.NewReader(input))
+	assertNoError(t, err)
+
+	assertIntEquals(t, 165, Part1(parsed))
+}
+
+// Helper
+func assertTrue(t *testing.T, b bool) {
+	t.Helper()
+	if !b {
+		t.Fatal("Expected true")
+	}
+}
+
+func assertIntEquals(t *testing.T, expected int, actual int) {
+	t.Helper()
+	if expected != actual {
+		t.Fatalf("Expected %d, got %d", expected, actual)
+	}
+}
+
+func assertStringEquals(t *testing.T, expected string, actual string) {
+	t.Helper()
+	if expected != actual {
+		t.Fatalf("Expected '%s', got '%s'", expected, actual)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+}
